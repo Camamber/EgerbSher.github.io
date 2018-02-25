08759937a00775d0b9fd87aea11515df3361c791
unknown <egorka.k2009@ukr.net>
2018-02-25 15:10:12 +0200

    anim

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    var intro = $('.intro'),
+        reg = $('.reg');
+
+    setTimeout(function () {
+           intro.hide(500);
+           reg.show(500);
+    }, 4000);
+});
+

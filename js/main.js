f02db22b68c4af99d960661e1c28a590d40b432c
EgerbSher <egorka.k2009@ukr.net>
2018-02-27 00:49:56 +0200

    rework main screen

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 $(function () {
-    var intro = $('.intro'),
-        reg = $('.reg');
+    var reg = $('.reg');
 
     setTimeout(function () {
-        intro.css({position: 'fixed'});
-        reg.css({position: 'fixed'});
-        intro.hide(500);
-        reg.show(500, function() {
-        reg.css({position: 'inherit'});
-  });
+        reg.fadeIn(500);
     }, 4000);
 
-
-    $(".reg-button a").hover(function() {
-        $(".reg-button").css("background", "url(\"img/mazok.png\") center no-repeat");
-        $(".reg-button").css("background-size","auto 12vmax");
-    },
-    function() {
-        $(".reg-button").css({background: "none"});
-    });
 });
 
 
